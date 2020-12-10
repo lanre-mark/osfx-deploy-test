f2fc78915669da6ad3cdbeb924b936aea09114d8
olanrewaju Makinde <lanre.developer@gmail.com>
2020-12-10 23:05:45 +0100

    refactoring yamls

--- cmd/osfx-exec.bat
+++ cmd/osfx-exec.bat
@@ -6,8 +6,38 @@ kubectl apply -f 1_osfx_loadbalancer.yaml
 kubectl apply -f 3_osfx_defaults.yaml
 
 kubectl create namespace cert-manager
+kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yaml
 kubectl apply -f 8_osfx_issuer_live.yaml
 kubectl apply -f 9_osfx_ingress.yaml
 kubectl describe ingress
 kubectl describe certificate
+
+
+
+
+cat <<EOF > test-cert-manager-resources.yaml
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: cert-manager-test
+---
+apiVersion: certmanager.k8s.io/v1alpha1
+kind: Issuer
+metadata:
+  name: test-selfsigned
+  namespace: cert-manager-test
+spec:
+  selfSigned: {}
+---
+apiVersion: certmanager.k8s.io/v1alpha1
+kind: Certificate
+metadata:
+  name: selfsigned-cert
+  namespace: cert-manager-test
+spec:
+  commonName: example.com
+  secretName: selfsigned-cert-tls
+  issuerRef:
+    name: test-selfsigned
+EOF
