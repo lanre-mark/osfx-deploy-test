a5b24d733c0217e5d859478e57bd82d0c6898b9e
olanrewaju Makinde <lanre.developer@gmail.com>
2020-12-10 15:55:13 +0100

    new issues

--- cmd/osfx-exec.bat
+++ cmd/osfx-exec.bat
@@ -1,2 +1,13 @@
 openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout osfx-key.pem -out osfx-cert.pem
 kubectl create secret tls osfx-cert --key osfx-key.pem --cert osfx-cert.pem
+
+kubectl apply -f 0_osfx_mandatory.yaml
+kubectl apply -f 1_osfx_loadbalancer.yaml
+kubectl apply -f 3_osfx_defaults.yaml
+
+kubectl create namespace cert-manager
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.12.0/cert-manager.yaml
+kubectl create -f 8_osfx_issuer_live.yaml
+kubectl apply -f 9_osfx_ingress.yaml
+kubectl describe ingress
+kubectl describe certificate
