4a7f8f183688dd98a90865a7d9edb9452c528efd
olanrewaju Makinde <lanre.developer@gmail.com>
2020-12-11 01:54:06 +0100

    troubleshooting

--- cmd/osfx-exec.bat
+++ cmd/osfx-exec.bat
@@ -7,7 +7,7 @@ kubectl apply -f 3_osfx_defaults.yaml
 
 kubectl create namespace cert-manager
 kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager-no-webhook.yaml
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yaml
 kubectl apply -f 8_osfx_issuer_live.yaml
 kubectl apply -f 9_osfx_ingress.yaml
 kubectl describe ingress
