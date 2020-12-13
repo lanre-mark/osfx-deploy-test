213c278e2bbb2833450cc38742d856a134152de1
olanrewaju Makinde <lanre.developer@gmail.com>
2020-12-13 14:03:35 +0100

    added jetstack and calicco CNI

--- cmd/osfx-exec.bat
+++ cmd/osfx-exec.bat
@@ -10,9 +10,15 @@ kubectl apply -f 0_osfx_mandatory.yaml
 kubectl apply -f 1_osfx_loadbalancer.yaml
 kubectl apply -f 3_osfx_defaults.yaml
 
+** https://cert-manager.io/docs/installation/kubernetes/ **
 kubectl create namespace cert-manager
-kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yaml
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --version v1.1.0 \
+  --set installCRDs=true
 kubectl apply -f 8_osfx_issuer_live.yaml
 kubectl apply -f 9_osfx_ingress.yaml
 kubectl describe ingress
@@ -21,28 +27,3 @@ kubectl describe certificate
 
 
 
-cat <<EOF > test-cert-manager-resources.yaml
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: cert-manager-test
----
-apiVersion: certmanager.k8s.io/v1alpha1
-kind: Issuer
-metadata:
-  name: test-selfsigned
-  namespace: cert-manager-test
-spec:
-  selfSigned: {}
----
-apiVersion: certmanager.k8s.io/v1alpha1
-kind: Certificate
-metadata:
-  name: selfsigned-cert
-  namespace: cert-manager-test
-spec:
-  commonName: example.com
-  secretName: selfsigned-cert-tls
-  issuerRef:
-    name: test-selfsigned
-EOF
